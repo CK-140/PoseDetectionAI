f0ca33c01570f248e6fd1b6c8f468a20431024b7
CK-140 <chitwan.kasana@gmail.com>
2021-05-07 09:38:38 +0530

    UI fix

--- app/src/main/java/com/google/mlkit/vision/demo/kotlin/LoginActivity.kt
+++ app/src/main/java/com/google/mlkit/vision/demo/kotlin/LoginActivity.kt
@@ -1,43 +1,116 @@
 package com.google.mlkit.vision.demo.kotlin
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.text.TextUtils
+import android.util.Log
+import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textview.MaterialTextView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.mlkit.vision.demo.R
-import com.google.mlkit.vision.demo.databinding.ActivityLoginBinding
+//import com.google.mlkit.vision.demo.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityLoginBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-         var f1: FirebaseAuth
-         f1 = FirebaseAuth.getInstance()
 
-        binding.signupnew.setOnClickListener(View.OnClickListener {
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+        var f1: FirebaseAuth
+        f1 = FirebaseAuth.getInstance()
+        val lowkeyUseful: TextView = findViewById(R.id.useless_textview2)
+        val loginbutton:MaterialButton= findViewById(R.id.login_btn)
+        val signupyo:MaterialTextView=findViewById(R.id.signupnew)
+        val emailtxt:TextInputEditText=findViewById(R.id.login_email)
+        val passtxt:TextInputEditText=findViewById(R.id.login_password)
+
+        signupyo.setOnClickListener(View.OnClickListener {
             gotoSignup()
         })
 
-        binding.loginBtn.setOnClickListener(View.OnClickListener {
-            var email: String = binding.loginEmail.text.toString()
-            var pass: String = binding.loginPassword.text.toString()
-            if(email.isEmpty()) binding.loginEmail.setError("Enter your email")
-            if(pass.isEmpty() || pass.length < 6) binding.loginPassword.setError("At least 6 characters")
+        lowkeyUseful.setOnTouchListener(object : View.OnTouchListener {
+            var handler: Handler = Handler()
+            var numberOfTaps = 0
+            var lastTapTimeMs: Long = 0
+            var touchDownMs: Long = 0
+            override fun onTouch(v: View?, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> touchDownMs = System.currentTimeMillis()
+                    MotionEvent.ACTION_UP -> {
+                        // Handle the numberOfTaps
+                        handler.removeCallbacksAndMessages(null)
+                        if (System.currentTimeMillis() - touchDownMs
+                            > ViewConfiguration.getTapTimeout()) {
+                            //it was not a tap
+                            numberOfTaps = 0
+                            lastTapTimeMs = 0
+                        }
+                        if (numberOfTaps > 0
+                            && System.currentTimeMillis() - lastTapTimeMs
+                            < ViewConfiguration.getDoubleTapTimeout()
+                        ) {
+                            // if the view was clicked once
+                            numberOfTaps += 1
+                        } else {
+                            // if the view was never clicked
+                            numberOfTaps = 1
+                        }
+                        lastTapTimeMs = System.currentTimeMillis()
 
-          f1.signInWithEmailAndPassword(email,pass).addOnCompleteListener(OnCompleteListener {
-              if(it.isSuccessful){
-                  Toast.makeText(this,"Signed In", Toast.LENGTH_SHORT).show()
-                  gotoPoseCam()
-              }
-              else Toast.makeText(this,"Error Signing in", Toast.LENGTH_SHORT).show()
-          })
+                        // Triple Tap
+                        if (numberOfTaps == 4) {
+                            gotoPoseCam()
+                            // Double tap
+                        } else if (numberOfTaps == 2) {
+                            handler.postDelayed(Runnable {
+                                Toast.makeText(applicationContext, "Stop",
+                                    Toast.LENGTH_SHORT)
+                                    .show()
+                            }, ViewConfiguration.getDoubleTapTimeout().toLong())
+                        }
+                    }
+                }
+                return true
+            }
+        })
+
+        loginbutton.setOnClickListener(View.OnClickListener {
+            var email: String = emailtxt.text.toString()
+            var pass: String = passtxt.text.toString()
+
+            if(email.isEmpty()){ emailtxt.setError("Enter your email")
+            return@OnClickListener
+            }
+            if(pass.isEmpty()){
+                passtxt.setError("Enter A password")
+                return@OnClickListener
+            }
+
+
+            f1.signInWithEmailAndPassword(email,pass).addOnCompleteListener(OnCompleteListener {
+                if(it.isSuccessful){
+                    Toast.makeText(this,"Signed In", Toast.LENGTH_SHORT).show()
+                    gotoPoseCam()
+                }
+                else Toast.makeText(this,"Error Signing in", Toast.LENGTH_SHORT).show()
+            })
+
         })
 
     }
@@ -49,4 +122,6 @@ class LoginActivity : AppCompatActivity() {
         var intent1 = Intent(this,LivePreviewActivity::class.java)
         startActivity(intent1)
     }
+
+
 }
--- app/src/main/java/com/google/mlkit/vision/demo/kotlin/SignupActivity.kt
+++ app/src/main/java/com/google/mlkit/vision/demo/kotlin/SignupActivity.kt
@@ -4,47 +4,56 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.FirebaseAuth
 import com.google.mlkit.vision.demo.R
-import com.google.mlkit.vision.demo.databinding.ActivitySignupBinding
+//import com.google.mlkit.vision.demo.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySignupBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignupBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
+        setContentView(R.layout.activity_signup)
         var f1: FirebaseAuth
         f1 = FirebaseAuth.getInstance()
 
-        binding.signUpButon.setOnClickListener(View.OnClickListener {
-            var name: String = binding.userName.text.toString()
-            var email: String = binding.editTextTextEmailAddress.text.toString()
-            var pass: String = binding.editTextTextPassword.text.toString()
+        val signupbtn:Button=findViewById(R.id.sign_up_buton)
+        val uname:EditText=findViewById(R.id.user_name)
+        val emaile:EditText=findViewById(R.id.editTextTextEmailAddress)
+        val passe:EditText=findViewById(R.id.editTextTextPassword)
 
-            if (name.isEmpty() || name.length < 4) {
-                binding.userName.setError("Enter a valid 4 character name")
+        signupbtn.setOnClickListener(View.OnClickListener {
+            var name:String = uname.text.toString()
+            var email:String = emaile.text.toString()
+            var pass:String = passe.text.toString()
+
+            if(name.isEmpty() || name.length < 4) {
+                uname.setError("Enter a valid 4 character name")
+                return@OnClickListener
             }
-            if (email.isEmpty()) binding.editTextTextEmailAddress.setError("Enter valid Email")
+            if (email.isEmpty()) {
+                emaile.setError("Enter valid Email")
+                return@OnClickListener
+            }
             if (pass.isEmpty() || pass.length < 6) {
-                binding.editTextTextPassword.setError("At least 6 characters")
+                passe.setError("Enter password, 6 characters min")
+                return@OnClickListener
             }
             f1.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(OnCompleteListener {
-            if(it.isSuccessful){
-                Toast.makeText(this,"Signed up successfully",Toast.LENGTH_SHORT).show()
-                gotoposeCam()
-            }
+                if(it.isSuccessful){
+                    Toast.makeText(this,"Signed up successfully",Toast.LENGTH_SHORT).show()
+                    gotoposeCam()
+                }
                 else{
                     Toast.makeText(this,"Error, Try Again", Toast.LENGTH_SHORT).show()
-            }
+                }
             })
+
         })
-
-
     }
     fun gotoposeCam(){
         val intent = Intent(this,LivePreviewActivity::class.java)
